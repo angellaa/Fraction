7c364a5bec5dcda1d7f770673f3c9fa1401dedc6
Andrea Angella <angella.andrea@gmail.com>
2016-10-14 23:31:24 +0100

    Group tests using inner classes
    This looks nice in the test runner. I could have achieve the same
    grouping using regions even without the benefit of the test runner.

--- Fraction.Tests/FractionTests.cs
+++ Fraction.Tests/FractionTests.cs
@@ -5,132 +5,141 @@ namespace AndreaAngella.Tests
 {
     public class FractionTests
     {
-        [Test]
-        public void FractionZero_Plus_FractionZero()
+        public class AdditionTests
         {
-            Assert.That(new Fraction(0) + new Fraction(0), Is.EqualTo(new Fraction(0)));
+            [Test]
+            public void FractionZero_Plus_FractionZero()
+            {
+                Assert.That(new Fraction(0) + new Fraction(0), Is.EqualTo(new Fraction(0)));
+            }
+
+            [Test]
+            public void Zero_Plus_FractionZero()
+            {
+                Assert.That(0 + new Fraction(0), Is.EqualTo(new Fraction(0)));
+            }
+
+            [Test]
+            public void FractionZero_Plus_Zero()
+            {
+                Assert.That(new Fraction(0) + 0, Is.EqualTo(new Fraction(0)));
+            }
+
+            [Test]
+            public void FractionFour_Plus_FractionNine()
+            {
+                Assert.That(new Fraction(4) + new Fraction(9), Is.EqualTo(new Fraction(13)));
+            }
+
+            [Test]
+            public void Four_Plus_FractionNine()
+            {
+                Assert.That(4 + new Fraction(9), Is.EqualTo(new Fraction(13)));
+            }
+
+            [Test]
+            public void FractionFour_Plus_Nine()
+            {
+                Assert.That(new Fraction(4) + 9, Is.EqualTo(new Fraction(13)));
+            }
+
+            [Test]
+            public void Zero_Plus_OneOverTwo()
+            {
+                Assert.That(0 + new Fraction(1, 2), Is.EqualTo(new Fraction(1, 2)));
+            }
         }
 
-        [Test]
-        public void Zero_Plus_FractionZero()
+        public class ToStringTests
         {
-            Assert.That(0 + new Fraction(0), Is.EqualTo(new Fraction(0)));
+            [Test]
+            public void OneOverTwo_ToString()
+            {
+                Assert.That(new Fraction(1, 2).ToString(), Is.EqualTo("1/2"));
+            }
+
+            [Test]
+            public void Zero_ToString()
+            {
+                Assert.That(new Fraction(0).ToString(), Is.EqualTo("0"));
+            }
+
+            [Test]
+            public void ZeroOverOne_ToString()
+            {
+                Assert.That(new Fraction(0, 1).ToString(), Is.EqualTo("0"));
+            }
+
+            [Test]
+            public void Two_ToString()
+            {
+                Assert.That(new Fraction(2).ToString(), Is.EqualTo("2"));
+            }
+
+            [Test]
+            public void TwoOverOne_ToString()
+            {
+                Assert.That(new Fraction(2, 1).ToString(), Is.EqualTo("2"));
+            }
         }
 
-        [Test]
-        public void FractionZero_Plus_Zero()
+        public class EqualityTests
         {
-            Assert.That(new Fraction(0) + 0, Is.EqualTo(new Fraction(0)));
-        }
+            [Test]
+            public void Zero_Equals_FractionZero()
+            {
+                Assert.That((Fraction)0, Is.EqualTo(new Fraction(0)));
+            }
 
-        [Test]
-        public void FractionFour_Plus_FractionNine()
-        {
-            Assert.That(new Fraction(4) + new Fraction(9), Is.EqualTo(new Fraction(13)));
-        }
+            [Test]
+            public void FractionZero_Equals_FractionZero()
+            {
+                Assert.That(new Fraction(0), Is.EqualTo(new Fraction(0)));
+            }
 
-        [Test]
-        public void Four_Plus_FractionNine()
-        {
-            Assert.That(4 + new Fraction(9), Is.EqualTo(new Fraction(13)));
-        }
+            [Test]
+            public void FractionZeroOverOne_Equals_FractionZero()
+            {
+                Assert.That(new Fraction(0, 1), Is.EqualTo(new Fraction(0)));
+            }
 
-        [Test]
-        public void FractionFour_Plus_Nine()
-        {
-            Assert.That(new Fraction(4) + 9, Is.EqualTo(new Fraction(13)));
-        }
+            [Test]
+            public void Five_Equals_FractionFive()
+            {
+                Assert.That((Fraction)5, Is.EqualTo(new Fraction(5)));
+            }
 
-        [Test]
-        public void Zero_Plus_OneOverTwo()
-        {
-            Assert.That(0 + new Fraction(1, 2), Is.EqualTo(new Fraction(1, 2)));
-        }
+            [Test]
+            public void FractionFive_Equals_FractionFive()
+            {
+                Assert.That(new Fraction(5), Is.EqualTo(new Fraction(5)));
+            }
 
-        [Test]
-        public void OneOverTwo_ToString()
-        {
-            Assert.That(new Fraction(1, 2).ToString(), Is.EqualTo("1/2"));
-        }
+            [Test]
+            public void FractionFiveOverOne_Equals_FractionFive()
+            {
+                Assert.That(new Fraction(5, 1), Is.EqualTo(new Fraction(5)));
+            }
 
-        [Test]
-        public void Zero_ToString()
-        {
-            Assert.That(new Fraction(0).ToString(), Is.EqualTo("0"));
-        }
+            [Test]
+            public void Fraction_EqualsTo_ItSelf()
+            {
+                var fraction = new Fraction(5, 1);
 
-        [Test]
-        public void ZeroOverOne_ToString()
-        {
-            Assert.That(new Fraction(0, 1).ToString(), Is.EqualTo("0"));
-        }
+                Assert.That(fraction, Is.EqualTo(fraction));
+            }
 
-        [Test]
-        public void Two_ToString()
-        {
-            Assert.That(new Fraction(2).ToString(), Is.EqualTo("2"));
-        }
+            [Test]
+            public void Fraction_NotEqualsTo_Null()
+            {
+                Assert.That(new Fraction(5, 1), Is.Not.EqualTo(null));
+            }
 
-        [Test]
-        public void TwoOverOne_ToString()
-        {
-            Assert.That(new Fraction(2, 1).ToString(), Is.EqualTo("2"));
-        }
-
-        [Test]
-        public void Zero_Equals_FractionZero()
-        {
-            Assert.That((Fraction)0, Is.EqualTo(new Fraction(0)));
-        }
-
-        [Test]
-        public void FractionZero_Equals_FractionZero()
-        {
-            Assert.That(new Fraction(0), Is.EqualTo(new Fraction(0)));
-        }
-
-        [Test]
-        public void FractionZeroOverOne_Equals_FractionZero()
-        {
-            Assert.That(new Fraction(0, 1), Is.EqualTo(new Fraction(0)));
-        }
-
-        [Test]
-        public void Five_Equals_FractionFive()
-        {
-            Assert.That((Fraction)5, Is.EqualTo(new Fraction(5)));
-        }
-
-        [Test]
-        public void FractionFive_Equals_FractionFive()
-        {
-            Assert.That(new Fraction(5), Is.EqualTo(new Fraction(5)));
-        }
-
-        [Test]
-        public void FractionFiveOverOne_Equals_FractionFive()
-        {
-            Assert.That(new Fraction(5, 1), Is.EqualTo(new Fraction(5)));
-        }
-
-        [Test]
-        public void Fraction_EqualsTo_ItSelf()
-        {
-            var fraction = new Fraction(5, 1);
-
-            Assert.That(fraction, Is.EqualTo(fraction));
-        }
-
-        [Test]
-        public void Fraction_NotEqualsTo_Null()
-        {
-            Assert.That(new Fraction(5, 1), Is.Not.EqualTo(null));
-        }
-
-        [Test]
-        public void Null_NotEqualsTo_Fraction()
-        {
-            Assert.That(null, Is.Not.EqualTo(new Fraction(5, 1)));
+            [Test]
+            public void Null_NotEqualsTo_Fraction()
+            {
+                Assert.That(null, Is.Not.EqualTo(new Fraction(5, 1)));
+            }
         }
 
         [Test]
