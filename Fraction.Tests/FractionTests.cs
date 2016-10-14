5eccb7d2560e22391a10e1ffc125b93a091fcdb5
Andrea Angella <angella.andrea@gmail.com>
2016-10-14 22:40:40 +0100

    Inline variables in tests seems more readable

--- Fraction.Tests/FractionTests.cs
+++ Fraction.Tests/FractionTests.cs
@@ -7,34 +7,19 @@ namespace AndreaAngella.Tests
         [Test]
         public void FractionZero_Plus_FractionZero()
         {
-            var f1 = new Fraction(0);
-            var f2 = new Fraction(0);
-
-            var result = new Fraction(0);
-
-            Assert.That(f1 + f2, Is.EqualTo(result));
+            Assert.That(new Fraction(0) + new Fraction(0), Is.EqualTo(new Fraction(0)));
         }
 
         [Test]
         public void Zero_Plus_FractionZero()
         {
-            var a = 0;
-            var b = new Fraction(0);
-
-            var result = new Fraction(0);
-
-            Assert.That(a + b, Is.EqualTo(result));
+            Assert.That(0 + new Fraction(0), Is.EqualTo(new Fraction(0)));
         }
 
         [Test]
         public void FractionZero_Plus_Zero()
         {
-            var a = new Fraction(0);
-            var b = 0;
-
-            var result = new Fraction(0);
-
-            Assert.That(a + b, Is.EqualTo(result));
+            Assert.That(new Fraction(0) + 0, Is.EqualTo(new Fraction(0)));
         }
     }
 }
